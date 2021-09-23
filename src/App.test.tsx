0991641177fcfd6f14b75ec23f03515191d7abc5
DM <mcnally@udel.edu>
2021-09-23 19:39:40 -0400

    fixed tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
+//test('renders learn react link', () => {
+  //render(<App />);
+describe('App', () => {
+  beforeEach(() => {
+    render(<App />);
+  })
   it ("should have unfinished on screen", () => {
     const linkElement = screen.queryByText(/unfinished/i);
     expect(linkElement).not.toBeInTheDocument();
-});
+  });
 });
