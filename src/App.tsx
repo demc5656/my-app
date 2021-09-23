481c88b4095db15964e873ac20299f88de6d5ce7
DM <mcnally@udel.edu>
2021-09-23 19:04:19 -0400

    fixed import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import { DiceViewer } from './components/diceViewer';
+import { DiceViewer } from './components/DiceViewer';
 import { Col, Row } from 'react-bootstrap';
 import { ControlPanel } from './components/ControlPanel';
 
