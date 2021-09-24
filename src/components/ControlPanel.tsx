478eff633c709b8389b96a1af214f103f24a1a6c
DM <mcnally@udel.edu>
2021-09-23 21:06:22 -0400

    columns fix i beg of you

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -34,13 +34,13 @@ export function ControlPanel ({set100, set20, set12, set10, set8, set6, set4}:
     function setNow4() {
         set4(roll(4))
     }
-    return <Col>
-        <Row><Button data-testid="100-button" onClick={setNow100} className="m-2">d100</Button></Row>
-        <Row><Button data-testid="20-button" onClick={setNow20} className="m-2">d20</Button></Row>
-        <Row><Button data-testid="12-button" onClick={setNow12} className="m-2">d12</Button></Row>
-        <Row><Button data-testid="10-button" onClick={setNow10} className="m-2">d10</Button></Row>
-        <Row><Button data-testid="8-button" onClick={setNow8} className="m-2">d8</Button></Row>
-        <Row><Button data-testid="6-button" onClick={setNow6} className="m-2">d6</Button></Row>
-        <Row><Button data-testid="4-button" onClick={setNow4} className="m-2">d4</Button></Row>
-        </Col>
+    return <Row>
+        <Col><Button data-testid="100-button" onClick={setNow100} className="m-2">d100</Button></Col>
+        <Col><Button data-testid="20-button" onClick={setNow20} className="m-2">d20</Button></Col>
+        <Col><Button data-testid="12-button" onClick={setNow12} className="m-2">d12</Button></Col>
+        <Col><Button data-testid="10-button" onClick={setNow10} className="m-2">d10</Button></Col>
+        <Col><Button data-testid="8-button" onClick={setNow8} className="m-2">d8</Button></Col>
+        <Col><Button data-testid="6-button" onClick={setNow6} className="m-2">d6</Button></Col>
+        <Col><Button data-testid="4-button" onClick={setNow4} className="m-2">d4</Button></Col>
+        </Row>
 }
