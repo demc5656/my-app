16ea7bd672247e86691abc4994b4390934eebc41
DM <mcnally@udel.edu>
2021-09-28 16:03:40 -0400

    Added font size changes

--- src/components/TotalViewer.tsx
+++ src/components/TotalViewer.tsx
@@ -13,16 +13,15 @@ export function TotalViewer ({total100Value, total20Value, total12Value, total10
     const temp = total100Value+total20Value+total12Value+total10Value+total8Value+total6Value+total4Value;
     //const [zero100, reset100] = useState<number()
     return <Row>
-        <Col><strong data-testid="total-100">{total100Value}</strong></Col>
-        <Col><strong data-testid="total-20">{total20Value}</strong></Col>
-        <Col><strong data-testid="total-12">{total12Value}</strong></Col>
-        <Col><strong data-testid="total-10">{total10Value}</strong></Col>
-        <Col><strong data-testid="total-8">{total8Value}</strong></Col>
-        <Col><strong data-testid="total-6">{total6Value}</strong></Col>
-        <Col><strong data-testid="total-4">{total4Value}</strong></Col>
+        <Col><h3 className="text-center" data-testid="total-100">{total100Value}</h3></Col>
+        <Col><h3 className="text-center" data-testid="total-20">{total20Value}</h3></Col>
+        <Col><h3 className="text-center" data-testid="total-12">{total12Value}</h3></Col>
+        <Col><h3 className="text-center" data-testid="total-10">{total10Value}</h3></Col>
+        <Col><h3 className="text-center" data-testid="total-8">{total8Value}</h3></Col>
+        <Col><h3 className="text-center" data-testid="total-6">{total6Value}</h3></Col>
+        <Col><h3 className="text-center" data-testid="total-4">{total4Value}</h3></Col>
         <Row>
-            <Col></Col>
-            <Col><strong data-testid="total-total">{temp}</strong></Col>
+            <h1 className="text-center" data-testid="total-total">{temp}</h1>
         </Row>
     </Row>
 }
