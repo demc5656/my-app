da23a6d708cdf6ad3caf333065df794c1fb33855
DM <mcnally@udel.edu>
2021-09-28 15:34:17 -0400

    tests fully added

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -53,6 +53,47 @@ describe('App', () => {
     expect(rolled4).not.toEqual(zero4);
   });
 
+  it ("should zero the value of the proper die when reset buttons are pressed", async () => {
+    const zero100 = screen.getByTestId("100").textContent;
+    const zero20 = screen.getByTestId("20").textContent;
+    const zero12 = screen.getByTestId("12").textContent;
+    const zero10 = screen.getByTestId("10").textContent;
+    const zero8 = screen.getByTestId("8").textContent;
+    const zero6 = screen.getByTestId("6").textContent;
+    const zero4 = screen.getByTestId("4").textContent;
+
+    const butt100 = screen.getByTestId("reset-100-button");
+    const butt20 = screen.getByTestId("reset-20-button");
+    const butt12 = screen.getByTestId("reset-12-button");
+    const butt10 = screen.getByTestId("reset-10-button");
+    const butt8 = screen.getByTestId("reset-8-button");
+    const butt6 = screen.getByTestId("reset-6-button");
+    const butt4 = screen.getByTestId("reset-4-button");
+
+    butt100.click();
+    const rolled100 = await screen.getByTestId("100").textContent;
+    butt20.click();
+    const rolled20 = await screen.getByTestId("20").textContent;
+    butt12.click();
+    const rolled12 = await screen.getByTestId("12").textContent;
+    butt10.click();
+    const rolled10 = await screen.getByTestId("10").textContent;
+    butt8.click();
+    const rolled8 = await screen.getByTestId("8").textContent;
+    butt6.click();
+    const rolled6 = await screen.getByTestId("6").textContent;
+    butt4.click();
+    const rolled4 = await screen.getByTestId("4").textContent;
+
+    expect(rolled100).toEqual(zero100);
+    expect(rolled20).toEqual(zero20);
+    expect(rolled12).toEqual(zero12);
+    expect(rolled10).toEqual(zero10);
+    expect(rolled8).toEqual(zero8);
+    expect(rolled6).toEqual(zero6);
+    expect(rolled4).toEqual(zero4);
+  });
+
   it ("should clear everything to 0 when mega-reset button is clicked", async () => {
     const zero100 = screen.getByTestId("100").textContent;
     const zero20 = screen.getByTestId("20").textContent;
